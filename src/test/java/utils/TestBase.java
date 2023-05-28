5175012646c494ffa2e7a0defb9249713c3cdc3a
vasudev0001 <vasudevkoppala01@gmail.com>
2023-05-28 10:56:18 +0530

    change in testBase

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -10,7 +10,7 @@ public class TestBase {
 		if (driver == null) {
 			System.out.println("driver null");
 
-			System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.manage().window().maximize();
 			driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");

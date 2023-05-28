5963cafff1d98fc747ce5198593a11a34c19252c
vasudev0001 <vasudevkoppala01@gmail.com>
2023-05-28 11:47:15 +0530

    Added properties file

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -1,19 +1,36 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
-	public WebDriver driver;
+	private WebDriver driver;
+	Properties prop;
 
 	public WebDriver getDriverInstance() {
-		if (driver == null) {
-			System.out.println("driver null");
 
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\chromedriver.exe");
-			driver = new ChromeDriver();
+		prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(
+					System.getProperty("user.dir") + "\\src\\test\\resources\\global.properties");
+			prop.load(fis);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (driver == null) {
+			if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver.exe");
+				driver = new ChromeDriver();
+			}
+
 			driver.manage().window().maximize();
-			driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+			driver.get(prop.getProperty("URL"));
 		}
 		return driver;
 	}
